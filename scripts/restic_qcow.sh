2510857f4932423cb675e425e8900298111c3a2c
TypicalAM <tpam_piaseczny@protonmail.com>
2025-07-25 17:50:20 +0200

    Replaced the restic script with an alias

--- scripts/restic_qcow.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-REPO="sftp:poznan-server:/mnt/drive/backups/restic_windows_images"
-FILE="/mnt/virt/images/win11/win11.qcow2"
-PASSWORD_FILE="/tmp/restic"
-
-pass ResticWindowsPassword >/tmp/restic
-restic -r $REPO --password-file $PASSWORD_FILE backup $FILE
-rm /tmp/restic
