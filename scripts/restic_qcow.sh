ee70038d630aab3d562690636399ebae09faaf86
TypicalAM <tpam_piaseczny@protonmail.com>
2025-07-25 16:02:26 +0200

    Restic via sftp

--- scripts/restic_qcow.sh
+++ scripts/restic_qcow.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-REPO="/mnt/temp/restic"
+REPO="sftp:poznan-server:/mnt/drive/backups/restic_windows_images"
 FILE="/mnt/virt/images/win11/win11.qcow2"
 PASSWORD_FILE="/tmp/restic"
 
-pass ResticPassword >/tmp/restic
-sshfs poznan-server:/mnt/drive/backups /mnt/temp
+pass ResticWindowsPassword >/tmp/restic
 restic -r $REPO --password-file $PASSWORD_FILE backup $FILE
-#umount /mnt/temp
+rm /tmp/restic
