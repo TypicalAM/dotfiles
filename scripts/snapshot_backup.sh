bef7d146b301620feb7af06c0e606ecfb3840708
TypicalAM <tpam_piaseczny@protonmail.com>
2025-11-17 21:54:35 +0100

    Snapshot backup tooling

---
+++ scripts/snapshot_backup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#
+# Backs up the latest RO snapper btrfs snapshot to a remote restic repo
+
+SNAPSHOT_DIR="/home/.snapshots"
+PASSWORD_FILE="/tmp/restic"
+REPO="/mnt/temp/restic_tygrys20"
+
+echof() {
+	local prefix="$1"
+	local message="$2"
+	case "$prefix" in
+	header) msgpfx="[\e[1;95m\e[m]" ;;
+	info) msgpfx="[\e[1;97m=\e[m]" ;;
+	act) msgpfx="[\e[1;92m*\e[m]" ;;
+	ok) msgpfx="[\e[1;93m+\e[m]" ;;
+	error) msgpfx="[\e[1;91m!\e[m]" ;;
+	*) msgpfx="" ;;
+	esac
+	echo -e "$msgpfx $message"
+}
+
+main() {
+	echof info "Backing up the latest snapshot"
+	sudo -u adam pass restic/snapshot-password > /tmp/restic
+	local filename=$(ls -t1 "$SNAPSHOT_DIR" | head -n 1)
+	local snapshotpath="$SNAPSHOT_DIR/$filename"
+	echof info "Using snapshot $snapshotpath"
+
+	echof info "Mounting backup drive"
+	sudo -u adam sshfs poznan-server:/mnt/drive/backups /mnt/temp -o allow_other
+	cd $snapshotpath
+	echof info "Backing up via restic"
+	restic -r $REPO --password-file $PASSWORD_FILE backup --exclude-file /var/home/adam/additional/dotfiles/scripts/snapshot_exclude snapshot
+	echof info "Done"
+	umount /mnt/temp
+}
+
+main "$*"
