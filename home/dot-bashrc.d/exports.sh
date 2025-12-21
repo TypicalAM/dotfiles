cb5c0e372f801202217d4baa535b56622c35e020
TypicalAM <tpam_piaseczny@protonmail.com>
2025-12-21 21:13:06 +0100

    Vagrant

--- home/dot-bashrc.d/exports.sh
+++ home/dot-bashrc.d/exports.sh
@@ -8,3 +8,4 @@ export FZF_DEFAULT_OPTS=" \
 --color=bg+:-1,bg:-1,spinner:#f4dbd6,hl:#ed8796 \
 --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
 --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
+export VAGRANT_DEFAULT_PROVIDER=libvirt
